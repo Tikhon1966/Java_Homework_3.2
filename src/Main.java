302ebe920129328946ebc2f9353e04da4633e5ac
Oleg <ovt1966@gmail.com>
2023-04-06 12:09:27 +0300

    Change Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
         double bodyWeight = 98; // вес в килограммах
-        double heightMan = 1.87; // рос в метрах
+        double heightMan = 1.87; // рост в метрах
         int index = service.calculate(bodyWeight, heightMan); // ИМЧ в целых числах
         System.out.println(index);
     }
