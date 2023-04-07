19538f61571a6154bef82e0ad6136259841b5867
Oleg <ovt1966@gmail.com>
2023-04-07 20:04:44 +0300

    Format cod

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,7 +1,7 @@
-public class BmiService{
+public class BmiService {
     int calculate(double weight, double height) {
         double index = weight / (height * height);
-        return (int)index;
+        return (int) index;
     }
 }
 
